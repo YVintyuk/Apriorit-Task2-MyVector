69a0f229cd58ac1d36506439ec9cb01fb780fbf5
Yuliana Vintyuk <yuliana.vintyuk@gmail.com>
2022-05-11 18:50:31 +0200

    Initial Commit

---
+++ Code_review/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Code_review
+//
+//  Created by Yuliana Vintyuk on 11.05.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
